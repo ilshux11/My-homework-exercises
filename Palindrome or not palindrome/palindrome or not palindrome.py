fd201ada17dc612da9a5965a5472510742ecb623
ilshux11 <fan-keksik94@yandex.ru>
2020-03-14 17:34:48 +0300

    Changed the strings of code

--- Palindrome or not palindrome/palindrome or not palindrome.py
+++ Palindrome or not palindrome/palindrome or not palindrome.py
@@ -1,18 +1,27 @@
-my_str = "Rise to vote, sir."
+my_string = "Rise to vote, sir."
+# or: my_string = input("Enter string: ")
 
-#define punction
+# define punction
 punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
 
-#remove punction from the string
-no_punct = ""
+# remove punction from the string
+no_punctuation = ""
 for char in my_str:
 	if char not in punctuations:
-		no_punct += char
+		no_punctuation += char
 
-no_punct.lower
-rev = no_punct[::-1]
+no_punctuation = string # change variable
 
-if no_punct == rev:
+# remove whitespaces
+new_string = " "
+for char in string:
+	if char == " ":
+		new_string += char
+
+new_string.lower
+reverse_string = new_string[::-1]
+
+if new_string == reverse_string:
 	print("\nYes, it's a palindrome")
 else:
 	print("\nNo, it isn't a palindrome")
